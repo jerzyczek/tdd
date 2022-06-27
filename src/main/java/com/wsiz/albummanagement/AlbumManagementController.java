4425bdc9c6e001870d1ff508f08f7d87c98d00c9
Mateusz Maciąg <mmaciag922@gmail.com>
2022-06-27 18:21:26 +0200

    5 test fix2

--- src/main/java/com/wsiz/albummanagement/AlbumManagementController.java
+++ src/main/java/com/wsiz/albummanagement/AlbumManagementController.java
@@ -1,12 +1,15 @@
 package com.wsiz.albummanagement;
 
 import java.util.Objects;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +26,21 @@ public class AlbumManagementController {
 
         this.albumManagementRepository.save(albumRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/album")
+    public ResponseEntity<AlbumRequest> editAlbumById(@RequestBody AlbumRequest albumRequest) {
+        Optional<AlbumRequest> albumRequestOptional = this.albumManagementRepository.findById(albumRequest.getId());
+
+        AlbumRequest albumRequestToSave = albumRequestOptional.get();
+
+        albumRequestToSave.setName(albumRequest.getName());
+        albumRequestToSave.setAuthor(albumRequest.getAuthor());
+
+        this.albumManagementRepository.save(albumRequestToSave);
+        Optional<AlbumRequest> albumRequestEdited = this.albumManagementRepository.findById(albumRequestToSave.getId());
+
+        return ResponseEntity.ok(albumRequestEdited.get());
     }
 
     private void validateAlbumData(final AlbumRequest albumRequest) {
