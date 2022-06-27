2c38ed0d220573475ad3dacdfd61f970079d9fed
Mateusz Maciąg <mateusz.maciag@pb.com>
2022-06-27 16:43:21 +0200

    1st test

--- src/test/java/com/wsiz/albummanagement/AlbummanagementApplicationTests.java
+++ src/test/java/com/wsiz/albummanagement/AlbummanagementApplicationTests.java
@@ -1,13 +1,16 @@
 package com.wsiz.albummanagement;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class AlbummanagementApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void saveAlbumWithValidDataShouldReturn200StatusCode() {
+
 	}
 
 }
