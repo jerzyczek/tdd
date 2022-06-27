6cb7d7a40887b9186bd158f88c87ee9c8af6143d
Mateusz Maciąg <mateusz.maciag@pb.com>
2022-06-27 16:39:46 +0200

    initial commit

---
+++ src/main/java/com/wsiz/albummanagement/AlbummanagementApplication.java
@@ -0,0 +1,13 @@
+package com.wsiz.albummanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbummanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbummanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsiz/albummanagement/AlbummanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsiz.albummanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbummanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
