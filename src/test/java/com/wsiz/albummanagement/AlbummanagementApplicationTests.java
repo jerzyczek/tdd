f1cafbfa65cdc402017b1c4cc1a10ce11e81f272
Mateusz Maciąg <mmaciag922@gmail.com>
2022-06-27 16:50:36 +0200

    1st test fix1

--- src/test/java/com/wsiz/albummanagement/AlbummanagementApplicationTests.java
+++ src/test/java/com/wsiz/albummanagement/AlbummanagementApplicationTests.java
@@ -10,7 +10,7 @@ class AlbummanagementApplicationTests {
 
 	@Test
 	public void saveAlbumWithValidDataShouldReturn200StatusCode() {
-
+		final AlbumRequest albumRequest = new AlbumRequest();
 	}
 
 }
